f774dd10be4aa9824459e0f28bd281271cbd14f4
IT <akinlabisamon15@gmail.com>
2022-05-31 10:13:16 +0100

    hello world

--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -38,27 +38,27 @@
 {{--    <div class="page-wrapper">--}}
         <div class="page-content">
                 <div class="row">
-{{--            <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3">--}}
-{{--                @foreach($pin as $pi)--}}
-{{--                <div class="col">--}}
-{{--                    <div class="card radius-10">--}}
-{{--                        <div class="card-body">--}}
-{{--                            <div class="d-flex align-items-center">--}}
-{{--                                <div>--}}
-{{--                                    <p class="mb-0 text-secondary">Primedata Company</p>--}}
-{{--                                    <h4 class="my-1">Ref No: 2J0997u100n036  ₦{{$pi['amount']}}</h4>--}}
-{{--                                    <h2 class="my-1"><b>PIN</b>: {{$pi['pin']}}</h2>--}}
-{{--                                    <h2 class="my-1">Serial No: {{$pi['serial']}}</h2>--}}
-{{--                                    <h2 class="my-1">Expiry Date: {{$pi['expiry']}}</h2>--}}
-{{--                                    <p class="mb-0 font-13 text-success"><i class="bx bxs-up-arrow align-middle"></i>Prime</p>--}}
-{{--                                </div>--}}
-{{--                                <div class="widgets-icons bg-light-success text-success ms-auto"><i class="bx bxs-wallet"></i>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                @endforeach--}}
+            <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3">
+                @foreach($pin as $pi)
+                <div class="col">
+                    <div class="card radius-10">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <p class="mb-0 text-secondary">Primedata Company</p>
+                                    <h4 class="my-1">Ref No: 2J0997u100n036  ₦{{$pi['amount']}}</h4>
+                                    <h2 class="my-1"><b>PIN</b>: {{$pi['pin']}}</h2>
+                                    <h2 class="my-1">Serial No: {{$pi['serial']}}</h2>
+                                    <h2 class="my-1">Expiry Date: {{$pi['expiry']}}</h2>
+                                    <p class="mb-0 font-13 text-success"><i class="bx bxs-up-arrow align-middle"></i>Prime</p>
+                                </div>
+                                <div class="widgets-icons bg-light-success text-success ms-auto"><i class="bx bxs-wallet"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
             </div>
         </div>
         </div>
